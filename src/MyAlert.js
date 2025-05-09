5e15fad304bc19b4946e35a3afbb2d569af88255
hale0904 <leduyha2809@gmail.com>
2025-05-09 14:40:35 +0700

    Cai dat babel plugin module resolver

---
+++ src/MyAlert.js
@@ -0,0 +1,3 @@
+export const MyAlert = () => {
+    alert('Say hi')
+}
---
+++ src/components/Button/index.js
@@ -0,0 +1,5 @@
+import { MyAlert } from "~/MyAlert";
+
+export function Button(){
+    return <button onClick={MyAlert}>Click me</button>;
+}
