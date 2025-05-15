eb52f01eb87d448b3e8cb27da602d2515ddac569
hale0904 <leduyha2809@gmail.com>
2025-05-15 20:27:37 +0700

    Xay dung UI phan Header (P4)

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCircleXmark, faSpinner, faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
+import { faCircleXmark, faSpinner, faMagnifyingGlass, faEllipsisVertical, faEarthAsia, faCircleQuestion, faKeyboard } from '@fortawesome/free-solid-svg-icons';
 import Tippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 
@@ -10,9 +10,26 @@ import {Wrapper as PopperWrapper} from '~/components/Popper';
 import styles from './Header.module.scss';
 import images from '~/assets/images';
 import AccountItem from '~/components/AccountItem';
+import Menu from '~/components/Popper/Menu';
 
 
 const cx = classNames.bind(styles);
+
+const MENU_ITEMS = [
+    {
+        icon: <FontAwesomeIcon icon={faEarthAsia}/>,
+        title: 'English',
+    },
+    {
+        icon: <FontAwesomeIcon icon={faCircleQuestion}/>,
+        title: 'Feedback and Help',
+        to: '/feedback'
+    },
+    {
+        icon: <FontAwesomeIcon icon={faKeyboard}/>,
+        title: 'Keyboard shortcuts',
+    }
+];
 
 function Header() {
 
@@ -32,7 +49,7 @@ function Header() {
                 </div>
                 <Tippy 
                     interactive
-                    visible={searchResult.length > 0}
+                    // visible={searchResult.length > 0}
                         render={attrs => (
                                 <div className={cx('search-result')} tabIndex="-1" {...attrs}>
                                     <PopperWrapper> 
@@ -61,6 +78,12 @@ function Header() {
                 <div className={cx('actions')}>
                     <Button text>Upload</Button>
                     <Button primary>Log in</Button>
+
+                    <Menu items={MENU_ITEMS}>  
+                        <button className={cx('more-button')}>
+                            <FontAwesomeIcon icon={faEllipsisVertical}/>
+                        </button>
+                    </Menu>
                 </div>
             </div>
         </header>
--- src/components/Popper/Wrapper.js
+++ src/components/Popper/Wrapper.js
@@ -3,8 +3,8 @@ import styles from './Popper.module.scss';
 
 const cx = classNames.bind(styles);
 
-function warpper({children}) {
-    return <div className={cx('wrapper')}>
+function warpper({children, className}) {
+    return <div className={cx('wrapper', className)}>
         {children}
     </div>
 }
